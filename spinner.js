e71ab7c15b75d39d700756b4c2773c68a2251508
cre8ture <kai@thebodyliterate.com>
2023-11-16 17:55:27 -0500

    ne

--- spinner.js
+++ spinner.js
@@ -1,34 +1,3 @@
-// const spinner = document.getElementById('value');
-// const div = document.getElementById('draggable');
-// const forwardButton = document.getElementById('forward');
-// const backwardButton = document.getElementById('backward');
-
-// let rotationSpinner = -3;
-
-// console.log(rotationSpinner, div);
-
-// forwardButton.addEventListener('click', () => {
-//     console.log("FORWARD")
-//     rotationSpinner += 10;
-//     spinner.style.transform = `rotate(${360}deg)`;
-//     div.style.transform = `rotate(${rotationSpinner}deg)`;
-// });
-
-// backwardButton.addEventListener('click', () => {
-//     console.log("BACKWARD")
-
-//     rotationSpinner -= 10;
-//     spinner.style.transform = `rotate(${-360}deg)`;
-//     div.style.transform = `rotate(${rotationSpinner}deg)`;
-// });
-
-// Define rotation variable in a scope accessible by both scripts
-// let rotation = -3;
-
-// In your dragging script
-rotation += (angle - startAngle) * (180 / Math.PI);
-applyTransform();
-
 // In your spinner script
 const spinner = document.getElementById('value');
 const div = document.getElementById('draggable');
